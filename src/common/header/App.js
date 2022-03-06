d7f7a2645ae01cff0e94cc24de10a455b775f06d
leo hsieh <bardiche4768@gmail.com>
2022-03-06 16:18:47 +0800

    test nvp

---
+++ src/common/header/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+export default function HeaderApp() {
+  return (
+    <section class="header">
+      <section class="language_toggle">
+        <a href="https://virtualyoutuber.fandom.com/wiki/Akai_Haato">🇯🇵 日本語 </a> 
+            | 
+        <a href="https://virtualyoutuber.fandom.com/wiki/Akai_Haato">🇺🇸 English </a> 
+      </section>
+
+      <element class="button">Current Event</element>
+      <element class="button">Previous Works</element>
+      <element class="button">Contact</element>
+
+    </section>
+  );
+}
+
