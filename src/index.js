590de35a60b7c74c3d7021028ca43bf156855467
leo hsieh <bardiche4768@gmail.com>
2022-03-08 19:32:03 +0800

    reorganised folder naming

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { CreateTopMarginedPageTitle, CreateHeader } from "./common/utils";
-import { CreateLogoBanner, CreateAboutAndBulletinColumns } from "./index/utils";
-import { merge } from "./common/utils";
+import { Column } from "./modules/column";
+import { CreateSubtitle, CreateHeader, 
+    CreateTopMarginedPageTitle, merge} from "./utils";
+import { Bulletin } from "./modules/bulletin";
+import { Image } from "./modules/Image";
 
 
 function CreatePage(){
@@ -22,4 +24,49 @@ ReactDOM.render(
   </StrictMode>,
   rootElement
 );
+
+
+function CreateLogoBanner() {
+    var img = new Image();
+    img.SetMarginTop("20px");
+    img.SetWidth("65%");
+
+    return img.get("fig/common/logo_banner.png");
+}
+
+
+function CreateAboutAndBulletinColumns(){
+    var cols = new Column(3);
+
+    cols.SetRowWidth("95%");
+    //cols.SetRatiosEqually();
+    cols.SetWidthRatios(25, 40, 40);
+    cols.SetMarginTop("20px");
+
+    var bulletin = new Bulletin("500px");
+    for(var i = 0; i < 100; ++i)
+        bulletin.append(i.toString());
+
+    cols.insert(0, CreateAboutColumn());
+    cols.insert(1, CreateSubtitle("Video Release"), bulletin.get());
+    cols.insert(2, CreateSubtitle("Announcement"), bulletin.get());
+
+    return cols.get();
+}
+
+function CreateAboutColumn(){
+    var components = [];
+    components.push(CreateSubtitle("About"));
+    var img = new Image();
+    img.SetWidth("75%");
+    img.SetCircle();
+
+    components.push(img.get("fig/common/pfp.jpg"));
+    
+
+    return components;
+}
+  
+
+
 
