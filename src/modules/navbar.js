0eeff2565019a77aa1038ac2dc03c0647a9b1810
leo hsieh <bardiche4768@gmail.com>
2022-03-14 23:42:46 +0800

    nav bar very nice

--- src/modules/navbar.js
+++ src/modules/navbar.js
@@ -9,8 +9,8 @@ export const NavbarDropdown = () =>{
 
 	return(
 		<>
-		<nav className="dropdown">
-			<button className="dropbtn" onClick={expandDropdown}>
+		<nav id = "top_bar" className="dropdown">
+			<button id = "menu_icon" className="dropbtn">
 				{img.get("fig/common/menu.png")}
 			</button>
 			<div id="nav_dropdown" className="dropdown-content">
@@ -27,16 +27,96 @@ export const NavbarDropdown = () =>{
 	);
 }
 
-export function expandDropdown() {
-	document.getElementById("nav_dropdown").classList.toggle("show");
+class ColourRGBA{
+	/**
+	* @param {number} r
+	* @param {number} g
+	* @param {number} b
+	* @param {number} a
+	*/
+	constructor(r, g, b, a){
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.a = a;
+	}
+
+	get(){
+		return "rgba(".concat(this.r,',',this.g,',',this.b,',',this.a,')');
+	}
+
+	/**
+	* @param {ColourRGBA} rgba 
+	*/
+	substract(rgba){
+		return new ColourRGBA(this.r-rgba.r,this.g-rgba.g,this.b-rgba.b,this.a-rgba.a);
+	}
+	/**
+	* @param {ColourRGBA} rgba 
+	*/
+	add(rgba){
+		return new ColourRGBA(this.r+rgba.r,this.g+rgba.g,this.b+rgba.b,this.a+rgba.a);
+	}
+	/**
+	* @param {number} x 
+	*/
+	multiply(x){
+		return new ColourRGBA(this.r*x,this.g*x,this.b*x,this.a*x);
+	}
+	/**
+	* @param {number} x 
+	*/
+	divide(x){
+		return this.multiply(1/x);
+	}
+	/**
+	* @param {number} a
+	*/
+	setA(a){
+		return this.a = a;
+	}
 }
 
-window.onclick = function(event){
-	if(event.target.matches(".centered_img"))
-		return;
-
-	for(let drop of document.getElementsByClassName("dropdown-content"))
-		if(drop.classList.contains("show"))
-			drop.classList.remove("show");
+function createNavbarKeyFrames(resolution){
+	var keyframes = [];
+	var skyBlue = new ColourRGBA(135,206,235,1);
+	for(var i = 0; i < resolution; ++i){
+		skyBlue.setA(i/resolution);
+		keyframes.push({background: "linear-gradient(90deg,"+skyBlue.get()+"0%,"+skyBlue.get()+"100%)"});
+	}
 		
+	const royalBlue = new ColourRGBA(65,105,255,1);
+	resolution*=3;
+	const increment = royalBlue.substract(skyBlue).divide(resolution);
+	const preffix = "linear-gradient(90deg,"+royalBlue.get()+"0%,";
+	for(var i = 0; i < resolution; ++i){
+		skyBlue = skyBlue.add(increment);
+		keyframes.push({background: preffix+skyBlue.get()+(100.0*i/resolution).toString()+"%)"});
+	}
+		
+		
+	//keyframes.push({background: royalBlue.get()});
+	return keyframes;
 }
+
+const keyframes = createNavbarKeyFrames(200);
+console.log(keyframes);
+const invKeyframes = createNavbarKeyFrames(200).reverse();
+
+var navbarExpanded = false;
+
+/**
+* @param {MouseEvent} event the event
+*/
+export function toggleNavbar(event) {
+	var topBar = document.getElementById("top_bar");
+	if(!navbarExpanded)
+		topBar.animate(keyframes, { duration: 800, fill: 'forwards', easing: 'ease-out'});
+	else
+		topBar.animate(invKeyframes, { duration: 800, fill: 'forwards', easing: 'ease-in'});
+
+	navbarExpanded = !navbarExpanded;
+	document.getElementById("nav_dropdown").classList.toggle("show");
+	
+}
+window.onclick = toggleNavbar;
