94c7c7dad483071f070d0520047c6fb96bcd067c
leo hsieh <bardiche4768@gmail.com>
2022-03-26 19:33:40 +0800

    prev work

--- src/pages/previous_projects.js
+++ src/pages/previous_projects.js
@@ -4,9 +4,10 @@ import InvertableColumn from "../modules/invertable_columns";
 import "../css/previous_project.css";
 import Image from "../modules/Image";
 import { merge, wrapDiv, wrapDivRecursive, wrapDivStyled } from "../utils";
-import { fadeInExplosiveLatched } from "../modules/defaults/entrance_effect";
+import { fadeInExplosiveLatched, fadeInDelayed } from "../modules/defaults/entrance_effect";
 import createFootNote from "../footnote";
 import TitledMediaText from "../modules/titled_media_text";
+import Boarder from "../modules/config/border";
 
 const PreviousWorks= () => {
   window.scrollTo(0, 0);
@@ -20,14 +21,16 @@ export default PreviousWorks;
 function createTabs(){
   var cols = new InvertableColumn();
   var img = new Image();
-  img.setWidth("85%");
+  img.setWidth("65%");
+  img.setCorner(Boarder.ALL, "15px");
 
-  const preffix = "fig/common/";
-  const filenames = ["place_holder.png", "place_holder.png"];
+  const preffix = "fig/previous_projects/";
+  const filenames = ["proj1.jpg", "proj2.jpg"];
   const link = "#/previous-works/proj";
-
+  const projNames = ["World Wide Tour Guide", "Haato's Birthday Parade"];
   for(var i = 0; i < 2; i++){
-    const image = wrapDivRecursive(["thumbnail-container", "button"], img.get(preffix+filenames[i]));
+    const fig = img.get(preffix+filenames[i]);
+    const image = wrapDivRecursive(["thumbnail-container", "button"],projNames[i], fig);
     cols.insert(i, <a href={link+(i+1).toString()}>{fadeInExplosiveLatched.get(image)}</a>);
   }
   
@@ -37,8 +40,9 @@ function createTabs(){
   titledContainer.setTitleColor(205, 92, 92, 1);
   titledContainer.setBodyColor(165, 42, 42,1);
   titledContainer.setRight();
+  const title = wrapDivRecursive(["title-container","title"], "Previous Works");
   //return fadeInExplosiveLatched.get(wrapDiv("thumbnail-container", "aaa"));
-  return wrapDiv("page-container",  titledContainer.get(cols.get()));
+  return wrapDiv("page-container", fadeInDelayed.get(title), cols.get());
 }
 
 
