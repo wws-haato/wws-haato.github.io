cf09bb5b85da6edb75737b628837d9c44525c3e9
leo hsieh <bardiche4768@gmail.com>
2022-03-31 19:27:45 +0800

    language toggle up to proj1

--- src/pages/previous_works_proj1.js
+++ src/pages/previous_works_proj1.js
@@ -1,23 +1,23 @@
-import React from "react";
-import TitledContainer from '../modules/titled_container';
-import InvertableColumn from "../modules/invertable_columns";
 import "../css/previous_works.css";
+import "../css/page_switcher.css";
+
 import Image from "../modules/Image";
 import Youtube from "../modules/youtube";
-import { displayAnimationQueries, merge, wrapDiv, wrapDivRecursive, wrapDivStyled, wrapLink } from "../utils";
-import { fadeInExplosiveLatched, fadeInDelayed, fadeInExplosive } from "../modules/defaults/entrance_effect";
+
 import TitledMediaText from "../modules/titled_media_text";
 import Border from "../config/border";
 import Slider from "../modules/slider";
 import ProjectTopBanner from "../modules/project_top_banner";
 import ProjectDetails from "../modules/project_details";
 import createFootNote from "../footnote";
-import ColourRGBA from "../config/colour_rgba";
 import ImageLinked from "../modules/Image_linked";
 import Column from "../modules/column";
-import { EntranceEffect } from "../modules/entrance_effect";
-import "../css/page_switcher.css";
 import PageSwithcer from "../modules/page_switcher";
+import articlesProject1 from "../articles/previous_works_proj1";
+
+import { EntranceEffect } from "../modules/entrance_effect";
+import {merge, wrapDiv, wrapDivStyled} from "../utils";
+import {fadeInExplosiveLatched, fadeInExplosive } from "../modules/defaults/entrance_effect";
 
 
 
@@ -97,117 +97,80 @@ function createTopBanner(){
     );
 
     topBanner.setGraphic(fadeInExplosiveLatched.get(slider.get()));
-    topBanner.setSuptitle("WWS Haato Project 1");
-    topBanner.setTitle("World Wide Tour Guide");
-    topBanner.appendTitledPassage("03. 03, 2021", 
-        "Haachama went on her journey of self-discovery");
-    topBanner.appendTitledPassage("09. 03, 2021", 
-        "WWS Haato teamed up to support Haato while on her break");
+    let topBannerTexts = articlesProject1[0];
+    topBanner.setSuptitle(topBannerTexts.suptitle);
+    topBanner.setTitle(topBannerTexts.title);
+    topBanner.setPassage(topBannerTexts.passage);
+
     return topBanner.get();
 }
 
 
 function createVideoDetails(){
-    var img = new Image();
-    img.setWidth("50%");
-
     const dir = "fig/previous_works/proj1/";
     var details = new ProjectDetails();
 
     details.setContourColor(0, 102, 204, 0.6);
-    details.setSuptitle("Video");
     details.setBackgroundImage("fig/background/3c.jpg");
-    details.emplace(ProjectDetails.SINGLE);
+
+    let videoTexts = articlesProject1[1];
+    details.setSuptitle(videoTexts.suptitle);
 
     var youtube = new Youtube();
     youtube.setWidth("65%");
     youtube.setCorner(Border.ALL, "10px");
-    details.append("Watch on Youtube", youtube.get("https://youtu.be/LLuqBMnfKJY"));
 
-    details.emplace(ProjectDetails.DUAL);
-    details.append("Description", img.get(dir+"desc_img.png"),
-        "This project features spot photos all around the world.", 
-        "Participants joined as the local tour guide to show Haachama around their hometown.", 
-        "We added Haachama and Haaton with the participant's name \
-        onto the photos and alligned them thematically."
-    );
-    details.append("Concept of World Wide", img.get(dir+"earth.png"), 
-        "We wanted to emphasize the word \"World-Wide.\"", 
-        "We clustered photos continent-wise to show that Haachama is \
-        truly a World Wide Idol and has fans everywhere. ", 
-        "In the end of the video we showed supportive messages collected from different continents."
-    );
+    details.setGraphic("video", youtube.get("https://youtu.be/LLuqBMnfKJY"));
+
+
+    var img = new Image();
+    img.setWidth("50%");
+
+    details.setGraphic("homography-frames", img.get(dir+"desc_img.png"));
+    details.setGraphic("earth", img.get(dir+"earth.png"));
     
+    for(let content of videoTexts.contents){
+        details.emplace(content.length);
+        for(let cell of content)
+            details.appendCell(cell);
 
+    }
+    
     return details.get();
 }
 
 
 function createWebsiteDetails(){
     var details = new ProjectDetails();
-
-    //details.setContourColor(35,93,58, 0.4);
     details.setContourColor(23, 81, 185, 0.8);
-    details.setSuptitle("Website");
     details.setBackgroundImage("fig/background/web.jpg");
-    
-    details.emplace(ProjectDetails.DUAL);
-    details.append("Description",  TitledMediaText.createButton(
+    details.setGraphic("view-website", TitledMediaText.createButton(
         "Visit website", "https://haatotabi.tk/home", 
         {background: "crimson", marginTop: "10%", marginBottom: "7%"}),
         "This part of the project is done by MASS and make sure to check out their latest project!", 
         "We present the supportive messages on the website"
     );
 
-    var img = new ImageLinked();
+    
     var waterMark = new Image();
     waterMark.setWidth("50%");
 
+    var img = new ImageLinked();
     img.setWidth("25%");
-    img.setWaterMark(waterMark.get("fig/common/icons/ext_link.png"));
-    details.append("MASS", img.get("fig/common/icons/mass.png", 
-        "https://twitter.com/ManoSquad"), 
-        "Manotomo Alliance Support Squad, aka MASS, \
-        is a community focused on supporting Hololivers from around the globe.", 
-    );
+    const dir = "fig/common/icons/";
+
+    img.setWaterMark(waterMark.get(dir+"ext_link.png"));
+    details.setGraphic("mass", img.get(dir+"mass.png"));
+
+    let videoTexts = articlesProject1[2];
+    details.setSuptitle(videoTexts.suptitle);
+
     
+    for(let content of videoTexts.contents){
+        details.emplace(content.length);
+        for(let cell of content)
+            details.appendCell(cell);
 
-    return details.get();
-}
-
-
-
-function createDetailsBackup(){
-    var img = new Image();
-    img.setWidth("50%");
-
-    const dir = "fig/previous_works/proj1/";
-    var details = new ProjectDetails();
-    details.setContourColor(181, 38, 59, 0.8);
-    details.setContentColor(245, 245, 220, 0.6);
-    details.setSuptitle("Video");
-    details.setBackgroundImage("fig/background/3c.jpg");
-    details.emplace(ProjectDetails.SINGLE);
-
-    var youtube = new Youtube();
-    youtube.setWidth("65%");
-    youtube.setCorner(Border.ALL, "10px");
-    details.append("Watch on Youtube", youtube.get("https://youtu.be/LLuqBMnfKJY"));
-
-    details.emplace(ProjectDetails.DUAL);
-    details.append("Description", img.get(dir+"desc_img.png"),
-        "This project features spot photos all around the world.", 
-        "Participant joins as the local tour guide to show Haachama around their hometown.", 
-        "We PhotoShoped Haachama and haaton with participant's name \
-         written on the wooden board onto the photos."
-    );
-    details.append("Concept of World Wide", img.get(dir+"earth.png"), 
-        "We wanted to emphasize the word \"World-Wide.\"", 
-        "We clustered photos continent-wise to show that Haachama's fanbase uniformly distributes \
-        over the world, featuring some supportive messages from the corresponding continents.", 
-        "The strongest idol deserves our world wide support!"
-    );
-    
-
+    }
     return details.get();
 }
