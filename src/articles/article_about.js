38cfeec54f78c90e7be5408967b746678e860bd9
HaachamaChama <bardiche4768@gmail.com>
2022-03-31 23:59:32 +0800

    revised about-us
    fix typo

--- src/articles/article_about.js
+++ src/articles/article_about.js
@@ -9,9 +9,7 @@ const articlesAbout = [
             はあとちゃんが長期休みに入る2021年の3月に立ち上げました。"
     }, 
     {
-        en: "World Wide Support for Haato, aka WWS Haato, is a non-profit project team \
-            mainly focusing on Haato projects that gather fans around the world. \
-            Our team has been set up since March, 2021, when Haato started her long break.", 
+        en: "We have so far produced our.", 
         jp: "今までに、初めての企画「ワールドワイドはあちゃまツアーガイド」[TL NOT COMPLETED]を完遂することができましたが、\
             これからは、もっとはあちゃまを応援する企画を引き続き作り上げるつもりです。"
     }
