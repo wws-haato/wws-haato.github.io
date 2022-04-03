f07e744bd5a9dd57a6ab7e3e348607d26e19c513
leo hsieh <bardiche4768@gmail.com>
2022-04-03 21:50:03 +0800

    curr event

--- src/articles/article_current_event.js
+++ src/articles/article_current_event.js
@@ -47,17 +47,11 @@ const articlesCurrentEvent = [
                         jp: "はじめに"
                     },
                     // graphicID=0 or simply omit: no graphic request for this cell
-                    // graphicID>0: requesting graphic
+                    graphicID: "vn",
                     passage: {
                         lines: [
                             {
-                                en: "Haachamachama! We are team WWS Haato, dedicated to fan-based \
-                                    projects for Haato which gathering fans all over the world! ", 
-                                jp: "はあちゃまっちゃま～私たちはWWS Haatoチーム、世界中の \
-                                    ファンを集めてはあちゃま関連のファンプロジェクトを中心として"
-                            },
-                            {
-                                en: "This year, as Haachama is strong enough to face the difficulties in \
+                                en: "As Haachama is strong enough to face the difficulties in \
                                     the future, we would like to present a visual novel for our strongest \
                                     idol Haachama as a birthday gift in 2022!", 
                                 jp: "活動しています。今年のはあちゃまはより強くなっていて、\
@@ -67,9 +61,8 @@ const articlesCurrentEvent = [
                             },
                            
                             {
-                                en: "We're going to recruit particapants from different field, including \
-                                    storywriting, programmars, illustrators, musicians and testers! \
-                                    Your contribution is always welcome!", 
+                                en: "We're looking for particapants to complete \
+                                    the project together! Your contribution is always welcome!", 
                                 jp: "現在、多くの分野から参加者を募集しています。\
                                     物語を充実させるライター、ゲーム根幹のプログラミング、\
                                     絵師、音楽制作の人とβテスターなどを探しています！ 先輩達の協力を、心よりお待ちしております。"
@@ -84,6 +77,22 @@ const articlesCurrentEvent = [
                     },
                     // graphicID=0 or simply omit: no graphic request for this cell
                     // graphicID>0: requesting graphic
+                    graphicID: "secret-note", 
+                    graphicPassage: [
+                        {
+                            en: "[1/3] Want to know the secret of an idol’s daily life? ", 
+                            jp: ""
+                        }, 
+                        {
+                            en: "[2/3] You’re invited to Haachama world!", 
+                            jp: ""
+                        }, 
+                        {
+                            en: ">>> [3/3] Become HAACHAMA NOW <<< 　 ", 
+                            jp: "", 
+                            style: {fontWeight: "bold"}
+                        }
+                    ], 
                     passage: {
                         lines: [
                             {
@@ -108,32 +117,247 @@ const articlesCurrentEvent = [
                         ]
                     }
                 }
+            ], 
+            [
+                {
+                    title: {
+                        en: "Participation", 
+                        jp: ""
+                    },
+                    // graphicID=0 or simply omit: no graphic request for this cell
+                    //graphicID: "vn",
+                    passage: {
+                        lines: [
+                            {
+                                en: "We are looking for storywriting, programmars, \
+                                    illustrators, musicians and testers! ", 
+                                jp: ""
+                            },
+                            {
+                                en: "If you have interest to be a part of the project, \
+                                    please checkout the following sections!", 
+                                jp: ""
+                            },
+                        ]
+                    }
+                }, 
             ]
         ]
     },
     {
         className: "project-details", 
         suptitle: {
-            en: "About the game", 
-            jp: "ゲーム概要"
+            en: "Programming", 
+            jp: "はじめに"
         }, 
         contents: [
             [
                 {
-                    /*title: {
-                        en: "Watch on Youtube", 
-                        jp: "[JP] Watch on Youtube"
+                    title: {
+                        en: "Programmers", 
+                        jp: ""
                     },
-                    */ 
-                    graphicID: 0, 
                     // graphicID=0 or simply omit: no graphic request for this cell
-                    // graphicID>0: requesting graphic
+                    graphicID: "programmer",
                     passage: {
-                        
+                        lines: [
+                            {
+                                en: "We’re planning to produce this game with Python, probably building with Pygame", 
+                                jp: ""
+                            },
+                            {
+                                en: "Also, we’ll set the project on Github, using Git for version control. \
+                                    Programmers need to check the documents frequently to keep up-to-date.",
+                                jp: ""
+
+                            }, 
+                            {
+                                en: "Providing suggestions on code, frameworks, and game systems is also highly appreciated.", 
+                                jp: ""
+                            }
+                        ]
                     }
                 }
             ]
         ]
-    } 
+    },
+    {
+        className: "project-details", 
+        suptitle: {
+            en: "Writers", 
+            jp: "jp"
+        }, 
+        contents: [
+            [
+                {
+                    title: {
+                        en: "Screen Writers", 
+                        jp: ""
+                    },
+                    // graphicID=0 or simply omit: no graphic request for this cell
+                    graphicID: "screenwriters",
+                    passage: {
+                        lines: [
+                            {
+                                en: "Screenwriters need to provide ideas and create new scenarios, \
+                                writing outlines for writers. ", 
+                                jp: ""
+                            },
+                            {
+                                en: "This part of work will need to take more effort than \
+                                others in the early phase of our project.",
+                                jp: "", 
+                                style: {fontWeight: "bold"}
+
+                            }
+                        ]
+                    }
+                }, 
+                {
+                    title: {
+                        en: "Writers", 
+                        jp: ""
+                    },
+                    // graphicID=0 or simply omit: no graphic request for this cell
+                    graphicID: "writers",
+                    passage: {
+                        lines: [
+                            {
+                                en: "Writers need to complete the text for the scenarios, \
+                                making them interesting and reasonable", 
+                                jp: ""
+                            }
+                        ]
+                    }
+                }
+            ], 
+            [
+                {
+                    title: {
+                        en: "Editors", 
+                        jp: ""
+                    },
+                    // graphicID=0 or simply omit: no graphic request for this cell
+                    graphicID: "Editors",
+                    passage: {
+                        lines: [
+                            {
+                                en: "Editors will need to find out any typos, \
+                                conflicting content and correct the grammar before handing texts to translators.", 
+                                jp: ""
+                            }
+                        ]
+                    }
+                }, 
+                {
+                    title: {
+                        en: "Translators", 
+                        jp: ""
+                    },
+                    // graphicID=0 or simply omit: no graphic request for this cell
+                    // graphicID>0: requesting graphic
+                    graphicID: "Translators", 
+                    passage: {
+                        lines: [
+                            {
+                                en: "Translators are expected to translate information between JP <-> EN members \
+                                at any time if the other groups need translation, and finished scripts (texts) to other languages. ", 
+                                jp: ""
+                            }, 
+                            {
+                                en: " You’ll have a lot of loads even later than editors.", 
+                                jp: "", 
+                                style: {fontWeight: "bold"}
+                            }
+                        ]
+                    }
+                }
+            ]
+        ]
+    },
+    {
+        className: "project-details", 
+        suptitle: {
+            en: "Artists", 
+            jp: "jp"
+        }, 
+        contents: [
+            [
+                {
+                    title: {
+                        en: "Illustrators", 
+                        jp: ""
+                    },
+                    // graphicID=0 or simply omit: no graphic request for this cell
+                    graphicID: "screenwriters",
+                    passage: {
+                        lines: [
+                            {
+                                en: "We’ll mainly collect character CG under certain rules to ensure the \
+                                art styles are kept in the same way. ",
+                                jp: ""
+                            }, 
+                            {
+                                en: "Some full illustrations with a certain theme are welcome as well, \
+                                please check the storyboard updates."
+                            }
+                        ]
+                    }
+                }, 
+                {
+                    title: {
+                        en: "Musicians", 
+                        jp: ""
+                    },
+                    // graphicID=0 or simply omit: no graphic request for this cell
+                    graphicID: "Musicians",
+                    passage: {
+                        lines: [
+                            {
+                                en: "We are planning to use free BGM mainly but any submission of BGM is appreciated. ",
+                                jp: ""
+                            }, 
+                            {
+                                en: "In addition to BGMs, \
+                                    we also would like to make an opening theme song for this game. ", 
+                                jp: ""
+                            }, 
+                            {
+                                en: "It would be great if we can make an ending song.", 
+                                jp:""
+                            }
+                        ]
+                    }
+                }
+            ], 
+            [
+                {
+                    title: {
+                        en: "Animators", 
+                        jp: ""
+                    },
+                    // graphicID=0 or simply omit: no graphic request for this cell
+                    graphicID: "Animators",
+                    passage: {
+                        lines: [
+                            {
+                                en: "For those visual novels, \
+                                we can use CGs in the game to make simple animations \
+                                if we have less power on this part. ", 
+                                jp: ""
+                            }, 
+                            {
+                                en: "Submission of a short 3D scene matching the song is welcome \
+                                if that doesn’t gonna kill you.", 
+                                jp:""
+                            }
+                        ]
+                    }
+                }
+            ]
+        ]
+    }
 ];
+
+
 export default articlesCurrentEvent;
--- src/pages/current_event.js
+++ src/pages/current_event.js
@@ -4,7 +4,7 @@ import InvertableColumn from "../modules/invertable_columns";
 import "../css/previous_works.css";
 import Image from "../modules/Image";
 import Youtube from "../modules/youtube";
-import { displayAnimationQueries, merge, wrapDiv, wrapDivRecursive, wrapDivStyled, wrapLanguages } from "../utils";
+import { displayAnimationQueries, merge, wrapDiv, wrapDivRecursive, wrapDivStyled, wrapLanguages, wrapStyle } from "../utils";
 import { fadeInExplosiveLatched, fadeInDelayed, fadeInExplosive } from "../modules/defaults/entrance_effect";
 import TitledMediaText from "../modules/titled_media_text";
 import Border from "../config/border";
@@ -18,6 +18,7 @@ import Column from "../modules/column";
 import { EntranceEffect } from "../modules/entrance_effect";
 import PageSwithcer from "../modules/page_switcher";
 import "../css/page_switcher.css";
+import "../css/project_details.css";
 
 import articlesCurrentEvent from "../articles/article_current_event";
 import LanguageSwitch from "../modules/language_switch";
@@ -29,8 +30,11 @@ const CurrentEvent = () => {
     window.scrollTo(0, 0);
     const App = merge(
         createTopBanner(), 
-        createOverview(),
-        createAboutTheGame(), 
+        createDescription(),
+        createProgramming(), 
+        createStoryboard(), 
+        createArtists(), 
+        //createAboutTheGame(), 
         createFootNote("70px")
     );
     EntranceEffect.startAllRequest();
@@ -41,14 +45,13 @@ const CurrentEvent = () => {
 
 export default CurrentEvent;
 
-
+const dir = "fig/current_event/";
 function createTopBanner(){
     let topBannerTexts = articlesCurrentEvent[0];
     var img = new Image();
     img.setCorner(Border.ALL, "10px");
     var topBanner = new ProjectTopBanner();
 
-    const dir = "fig/current_event/";
     topBanner.setGraphic(img.get(dir+"haachama_vn_thumb.jpg"));
     topBanner.setSuptitle(topBannerTexts.suptitle);
     topBanner.setTitle(topBannerTexts.title);
@@ -57,8 +60,76 @@ function createTopBanner(){
     return topBanner.get();
 }
 
-function createOverview(){
+function createDescription(){
     let videoTexts = articlesCurrentEvent[1];
+    var details = new ProjectDetails();
+
+    details.setContourColor(0, 82, 33, 0.4);
+    details.setSuptitle(videoTexts.suptitle);
+    details.setBackgroundImage("fig/background/green.png");
+
+    var notiImg = new Image();
+    notiImg.setWidth("35%");
+
+    var slider = new Slider();
+    slider.hideBar();
+    slider.setClickWidth("5VW");
+    for(let line of videoTexts.contents[0][1].graphicPassage){
+        console.log(line);
+        var args = {className: "line"};
+        if(line.style) 
+            args.style = line.style;
+        
+        slider.append(wrapDiv(["passage", args], 
+            notiImg.get(dir+"notification.png"), wrapLanguages(line)));
+    }
+        
+    details.setGraphic("secret-note", slider.get());
+    notiImg.setWidth("50%");
+    notiImg.setMargin(Border.ALL, "20px");
+
+    details.setGraphic("vn", notiImg.get(dir+"vn.png"));
+    for(let content of videoTexts.contents){
+        details.emplace(content.length);
+        for(let cell of content)
+            details.appendCell(cell);
+
+    }
+    
+    return details.get();
+}
+
+
+function createProgramming(){
+    let videoTexts = articlesCurrentEvent[2];
+    console.log(videoTexts);
+    var details = new ProjectDetails();
+
+    details.setContourColor(181, 38, 59, 0.6);
+    details.setSuptitle(videoTexts.suptitle);
+    details.setBackgroundImage("fig/background/red_chessboard.png");
+
+    var youtube = new Youtube();
+    youtube.setWidth("65%");
+    youtube.setCorner(Border.ALL, "10px");
+
+    details.setGraphic("programmers", youtube.get("https://youtu.be/aHt-fGy5BYQ"));
+    
+    for(let content of videoTexts.contents){
+        console.log(content);
+        details.emplace(content.length);
+        for(let cell of content)
+            details.appendCell(cell);
+
+    }
+    
+    return details.get();
+}
+
+
+function createStoryboard(){
+    let videoTexts = articlesCurrentEvent[3];
+    console.log(videoTexts);
     var details = new ProjectDetails();
 
     details.setContourColor(0, 82, 33, 0.4);
@@ -69,8 +140,37 @@ function createOverview(){
     youtube.setWidth("65%");
     youtube.setCorner(Border.ALL, "10px");
 
-    details.setGraphic(1, youtube.get("https://youtu.be/aHt-fGy5BYQ"));
+    details.setGraphic("programmers", youtube.get("https://youtu.be/aHt-fGy5BYQ"));
+    
     for(let content of videoTexts.contents){
+        console.log(content);
+        details.emplace(content.length);
+        for(let cell of content)
+            details.appendCell(cell);
+
+    }
+    
+    return details.get();
+}
+
+
+function createArtists(){
+    let videoTexts = articlesCurrentEvent[4];
+    console.log(videoTexts);
+    var details = new ProjectDetails();
+
+    details.setContourColor(181, 38, 59, 0.6);
+    details.setSuptitle(videoTexts.suptitle);
+    details.setBackgroundImage("fig/background/red_chessboard.png");
+
+    var youtube = new Youtube();
+    youtube.setWidth("65%");
+    youtube.setCorner(Border.ALL, "10px");
+
+    details.setGraphic("programmers", youtube.get("https://youtu.be/aHt-fGy5BYQ"));
+    
+    for(let content of videoTexts.contents){
+        console.log(content);
         details.emplace(content.length);
         for(let cell of content)
             details.appendCell(cell);
