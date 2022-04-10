19ca286f3c8971cd1ecfbb37c78c180d66897182
henry4204aaa <henry4204aaa@hotmail.com>
2022-04-10 21:27:45 +0800

    minor fixes

--- src/articles/article_home.js
+++ src/articles/article_home.js
@@ -12,16 +12,12 @@ const articlesHome = [
             /*Here the Game title, done */
         }, 
         description: {
-            en: "As Haachama is strong enough to face the difficulties that lie ahead in the future, \
-                we want to gift her something special for her birthday. \
-                This year we present our own visual novel where players can walk the path of our strongest idol, \
+            en: "This year we present our own visual novel where players can walk the path of our strongest idol, \
                 interact with others and become a star! \
                 We're going to recruit participants from different fields, including \
                 story writers, programmers, illustrators, musicians and testers! \
                 Your contribution is always welcome!",
-            jp: "今年のはあちゃまはより強くなっていて、この先にどんな困難があっても乗り越えられると考えています。\
-                そんな彼女に、何か特別な誕生日プレゼントを贈りたいと思っています。 \
-                今年にプレゼントとする、ビジュアルノベルの中に、プレイヤーは最強アイドルの物語を紡いで、 \
+            jp: "私たちは今年にプレゼントとするビジュアルノベルの中に、プレイヤーは最強アイドルの物語を紡いで、 \
                 他の人々と触れ合って、世界中に輝く一等星に目指そう！ \
                 現在、多くの分野から参加者を募集しています。 \
                 ライター、プログラマー、イラストレーター、ミュージシャンとテスターの人を探しています！ \
